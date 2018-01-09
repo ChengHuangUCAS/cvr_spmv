0d638eb6fadd5ab82f059aa43e401ae8335f8a06
ChengHuangUCAS <35071571+ChengHuangUCAS@users.noreply.github.com>
2018-01-09 19:11:12 +0800

    Update matrix_debug.h

--- matrix_debug.h
+++ matrix_debug.h
@@ -34,15 +34,21 @@ typedef struct cvr{
 	int nnz;
 }cvr_t; // compressed vactorization-oriented sparse row format
 
-extern void print_coo(coo_t *coo);
-extern void print_coo_n(coo_t *coo, int n);
-extern void print_csr(csr_t *csr);
-extern void print_csr_n(csr_t *csr, int n);
-extern void print_cvr(cvr_t *cvr);
-extern void print_cvr_n(cvr_t *cvr, int n);
+void print_coo(coo_t *coo);
+void print_coo_n(coo_t *coo, int n);
+void print_csr(csr_t *csr);
+void print_csr_n(csr_t *csr, int n);
+void print_cvr(cvr_t *cvr);
+void print_cvr_n(cvr_t *cvr, int n);
 
-extern void print_thread(int thread_num);
-extern void print_cvr_info(cvr_t *cvr);
+void print_thread(int thread_num);
+//void print_cvr_info(cvr_t *cvr);
+//void print_cvr_thread_info(cvr_t *cvr, int thread_num);
+
+void print_matrix(csr_t *csr);
+void print_vector(float *x, int n);
+
+void print_cvr_detail(cvr_t *cvr, int thread_num, int thread_nnz, int thread_nrow, int n_lanes);
 
 
 void print_coo_n(coo_t *coo, int n){
@@ -86,10 +92,11 @@ void print_cvr_n(cvr_t *cvr, int n){
 	int n_threads = 1;
 	printf("\ncvr format\n");
 	for(i = 0; i < n_threads; i++){
+		printf("   val_ptr: ");
 		for(j = 0; j < n / n_threads; j++){
 			printf("%.2f ", cvr->val_ptr[i][j]);
 		}
-		printf("    ");
+		printf("    \ncolidx_ptr: ");
 		for(j = 0; j < n / n_threads; j++){
 			printf("%4d ", cvr->colidx_ptr[i][j]);
 		}
@@ -108,6 +115,7 @@ void print_thread(int thread_num){
 	printf("this thread = %d\n", thread_num);
 }
 
+/*
 void print_cvr_info(cvr_t *cvr){
 	printf("\ncvr info:\n");
 	printf("   val_ptr = %x\n", cvr->val_ptr);
@@ -120,9 +128,72 @@ void print_cvr_info(cvr_t *cvr){
 
 void print_cvr_thread_info(cvr_t *cvr, int thread_num){
 	printf("\ncvr thread info:\n");
-    printf("cvr->val_ptr[%d] = %x, ", thread_num, cvr->val_ptr[thread_num]);
-    printf("cvr->colidx_ptr[%d] = %x, ", thread_num, cvr->colidx_ptr[thread_num]);
-    printf("cvr->rec_ptr[%d] = %x, ", thread_num, cvr->rec_ptr[thread_num]);
-    printf("cvr->tail_ptr[%d] = %x\n", thread_num, cvr->tail_ptr[thread_num]);
+	printf("cvr->val_ptr[%d] = %x, ", thread_num, cvr->val_ptr[thread_num]);
+	printf("cvr->colidx_ptr[%d] = %x, ", thread_num, cvr->colidx_ptr[thread_num]);
+	printf("cvr->rec_ptr[%d] = %x, ", thread_num, cvr->rec_ptr[thread_num]);
+	printf("cvr->tail_ptr[%d] = %x\n", thread_num, cvr->tail_ptr[thread_num]);
+}
+*/
+
+void print_matrix(csr_t *csr){
+	int i, j, col = 0;
+	printf("\nmatrix:\n");
+	for(i = 0; i < csr->nrow; i++){
+		for(j = 0; j < csr->ncol; j++){
+			if(col >= csr->row_ptr[i] && col < csr->row_ptr[i+1] && j == csr->col_idx[col]){
+				printf("%.2f ", csr->val[col]);
+				col++;
+			}else{
+				printf("0.00 ");
+			}
+		}
+		printf("\n");
+	}
+}
+void print_vector(float *x, int n){
+	int i;
+	printf("\nvector:\n");
+	for(i = 0; i < n; i++){
+		printf("%.2f \n", x[i]);
+	}
+}
+
+void print_cvr_detail(cvr_t *cvr, int thread_num, int thread_nnz, int thread_nrow, int n_lanes){
+	int i;
+	printf("\ncvr detail: thread %d\n", thread_num);
+
+	printf("  *val_ptr:    ");
+	for(i = 0; i < thread_nnz; i++){
+		printf("%.2f ", cvr->val_ptr[thread_num][i]);
+	}
+	printf("\n");
+
+	printf("  *colidx_ptr: ");
+	for(i = 0; i < thread_nnz; i++){
+		printf("%4d ", cvr->colidx_ptr[thread_num][i]);
+	}
+	printf("\n");
+
+	printf("  *rec_ptr: ");
+	for(i = 0; i < thread_nrow; i++){
+		printf("%2d ", cvr->rec_ptr[thread_num][2*i]);
+	}
+	printf("\n            ");
+	for(i = 0; i < thread_nrow; i++){
+		printf("%2d ", cvr->rec_ptr[thread_num][2*i+1]);
+	}
+	printf("\n");
+
+	printf("  lrrec_ptr: ");
+	printf("%d ", cvr->lrrec_ptr[thread_num]);
+	printf("\n");
+
+	printf("  *tail_ptr: ");
+	for(i = 0; i < n_lanes; i++){
+		printf("%d ", cvr->tail_ptr[thread_num][i]);
+	}
+	printf("\n");
+
+	printf("nrow = %d, ncol = %d, nnz = %d\n\n", cvr->nrow, cvr->ncol, cvr->nnz);
 }
 
